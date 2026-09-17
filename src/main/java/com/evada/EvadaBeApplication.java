1f6fba3020a5c851fd03dc0764121dcda09f0082
alsthdua107 <alsthdua107@gmail.com>
2026-09-17 23:57:52 +0900

    Chore: Spring Boot 기본 프로젝트 생성 및 초기 설정 (Gradle)

---
+++ src/main/java/com/evada/EvadaBeApplication.java
@@ -0,0 +1,13 @@
+package com.evada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvadaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvadaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evada/EvadaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvadaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
